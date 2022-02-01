fab90b88c0442630eecf908dac596e646d0cc162
Jérémie Regnault <jeremie.regnault@capgemini.com>
2022-02-01 14:29:47 +0100

    exercice : listes

--- 03_structures-de-données/exercices.py
+++ 03_structures-de-données/exercices.py
@@ -4,3 +4,14 @@ def exercice1(name: str, age: int):
     tuple contenant le nom et l'age fournis.
     """
     pass
+
+
+def exercice2(n: int):
+    """Exercice sur les listes.
+    Ecris le code nécessaire pour retourner la liste
+    des carrés des nombres de 1 à n inclus.
+
+    Exemple:
+    exercice2(5) -> [1, 4, 9, 16, 25]
+    """
+    pass
--- 03_structures-de-données/tests.py
+++ 03_structures-de-données/tests.py
@@ -7,6 +7,18 @@ class TestExercices(unittest.TestCase):
 
     def test_exercice1(self):
         self.assertEquals(("John", 25), e.exercice1("John", 25))
+        self.assertEquals(("Ivan", 57), e.exercice1("John", 25))
+    
+    def test_exercice2(self):
+        self.assertEquals(
+            [1, 4, 9, 16, 25],
+            e.exercice2(5)
+        )
+        self.assertEquals(
+            [1, 4, 9, 16, 25, 36, 49, 64, 91, 100],
+            e.exercice2(10)
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
